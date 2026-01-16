14820da84c9fdee2fe9eb470b2813f28c25da967
Teriyakii07 <mika.rentuaya1234@gmail.com>
2026-01-17 00:06:59 +0800

    Create footer.php

---
+++ view/others/footer.php
@@ -0,0 +1,23 @@
+<link rel="stylesheet" href="../view/assets/css/footer.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+
+<footer class="site-footer">
+        <div class="footer-content">
+            <div class="social-icons">
+                <a href="#"><i class="fa-brands fa-instagram"></i></a>
+                <a href="#" class="x-logo-link"><span class="x-text">𝕏</span></a>
+                <a href="#"><i class="fa-brands fa-facebook-f"></i></a>
+                <a href="#"><i class="fa-brands fa-discord"></i></a>
+            </div>
+
+            <div class="footer-links">
+                <a href="#">Home</a>
+                <a href="#">Services</a>
+                <a href="#">About</a>
+                <a href="#">Terms</a>
+                <a href="#">Privacy Policy</a>
+            </div>
+
+            <p class="copyright">Safe Scan © 2026</p>
+        </div>
+    </footer>
