15a394bc22227ec78d821b0f7447543dd9f4bdda
Teriyakii07 <mika.rentuaya1234@gmail.com>
2026-02-01 23:43:17 +0800

    Update login.php

--- app/views/auth/login.php
+++ app/views/auth/login.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Safe Scan - Login</title>
-    <link rel="stylesheet" href="public/assets/css/logincss/login.css">
+    <link rel="stylesheet" href="/assets/css/logincss/login.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <link href="https://fonts.googleapis.com/css2?family=Kanit:wght@300;400;500;600&display=swap" rel="stylesheet">
 </head>
@@ -88,7 +88,7 @@
                 </form>
 
 <!-- js script path -->
-                <script src="../Safe Scan/loginvalidation.js"></script>
+                <script src="/assets/js/loginvalidation.js"></script>
             </div>
         </div>
 
