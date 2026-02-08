fdd081721bd2a209d8ce3132b5e7468c8d7263fe
Teriyakii07 <mika.rentuaya1234@gmail.com>
2026-02-08 16:42:06 +0800

    Create upload.php

---
+++ app/views/pages/upload.php
@@ -0,0 +1,35 @@
+<?php 
+    require __DIR__ . '/../others/navigation.php';
+?>
+
+<title>SafeScan</title>
+<link rel="stylesheet" href="/assets/css/upload.css">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+<div class="dev-container">
+    <div class="dev-card">
+        
+        <div class="icon-wrapper">
+            <i class="fa-solid fa-laptop-code"></i>
+            <div class="gear-icon">
+                <i class="fa-solid fa-gear"></i>
+            </div>
+        </div>
+
+        <h1 class="dev-title">Work in Progress</h1>
+        
+        <p class="dev-message">
+            We are currently working hard to bring this feature to life.<br>
+            Stay tuned for updates!
+        </p>
+
+        <div class="button-group">
+            <a href="/home" class="btn-primary">
+                Return to Home
+            </a>
+        </div>
+
+    </div>
+</div>
+
+<?php require __DIR__ . '/../others/footer.php'; ?>
