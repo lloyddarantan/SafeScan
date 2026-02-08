4ea90f0ed801b316a666b3984e47e13af6f3b840
Teriyakii07 <mika.rentuaya1234@gmail.com>
2026-02-08 16:42:13 +0800

    Update appliances.php

--- app/views/pages/appliances.php
+++ app/views/pages/appliances.php
@@ -5,6 +5,7 @@ require __DIR__ . '/../others/navigation.php';
 <title>SafeScan</title>
 <link rel="stylesheet" href="/assets/css/appliances.css">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
 
 <div class="dashboard-container">
 
